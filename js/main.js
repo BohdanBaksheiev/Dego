b5ade3d598e2db98d4025fdb956b860df6c74b46
Bohdan Baksheiev <bogdanbacksheev@gmail.com>
2018-08-03 00:30:52 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+  $('.j-firstSlider').slick({
+  		autoplay: true,
+  		arrows: false,
+  		adaptiveHeight:true
+  });
+
+  $('.j-secondSlider').slick({
+  		autoplay: true,
+  		arrows: true,
+  		adaptiveHeight:true
+  });
+});
